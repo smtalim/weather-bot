15dc94ae85ae6aad2b65f4f348e7a4d88f927456
Sethupathi Asokan <sethu@joshsoftware.com>
2013-07-02 16:41:20 +0530

    Removed the test code

---
+++ forecast.rb
@@ -0,0 +1,15 @@
+require 'rest-client'
+require 'json'
+require 'logger'
+
+API_KEY = ENV['API_KEY']
+
+parsed = JSON.parse(RestClient.get(URI.encode("https://api.forecast.io/forecast/#{API_KEY}/37.4220773,-122.0829147")))
+log = Logger.new(STDOUT)
+log.level = Logger::INFO
+if parsed["currently"]["temperature"] > 60.0 
+  log.info "HIGH temperature today...(send SMS)"  
+else
+  log.info "OK"
+end
+
